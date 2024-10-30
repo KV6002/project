a92e7996285ac7d32cdbb3d451952ff45675cf3f
Hamza Ali <162442234+Hamza-Ali24@users.noreply.github.com>
2024-10-30 17:50:35 +0000

    Create db.js
    DB connection

---
+++ db.js
@@ -0,0 +1,28 @@
+// db.js
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+const connectToDatabase = async () => {
+  try {
+    if (!dbConnection) {
+      await client.connect();
+      dbConnection = client.db();
+      console.log("Successfully connected to MongoDB.");
+    }
+    return dbConnection;
+  } catch (error) {
+    console.error("Could not connect to MongoDB:", error);
+    process.exit(1); // exit the application if unable to connect
+  }
+};
+
+module.exports = connectToDatabase;
+ 
