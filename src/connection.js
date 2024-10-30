40bc24043b489d0474dc704db48be075829ccfc0
mattbrowne66 <132616056+mattbrowne66@users.noreply.github.com>
2024-10-30 17:01:20 +0000

    connection to database with nodejs added

---
+++ src/connection.js
@@ -0,0 +1,28 @@
+// connection.js
+const { MongoClient } = require('mongodb');
+
+// Replace this URI with your MongoDB connection string
+const uri = 'mongodb+srv://matt:northumbria@covid-data.8prc5.mongodb.net/';
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    // Select your database and collection
+    const database = client.db('covid');
+    const collection = database.collection('deaths');
+
+    // Sample data manipulation (fetching all documents)
+    const documents = await collection.find().toArray();
+    console.log('All Documents:', documents);
+
+  } finally {
+    // Ensure the client is closed when done
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
