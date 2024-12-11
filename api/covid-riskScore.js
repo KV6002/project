8ec1b2251af5f4d3d4fee4799de52acbe2cd7b0d
aliyaanahmed1 <aliyaanahmed@gozzby.co.uk>
2024-12-11 15:00:20 +0000

    Update to RiskLevel

--- api/covid-riskScore.js
+++ api/covid-riskScore.js
@@ -1,31 +1,32 @@
 const client = require('../db');
 
 module.exports = async (req, res) => {
-  // Add CORS headers
+  
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    res.status(204).end(); // Handle preflight requests
+    res.status(204).end(); 
     return;
   }
 
   try {
-    // Connect to the database
+   
     await client.connect();
-    const deathsCollection = client.db("COVID-New").collection("Risk Scores"); // Ensure no spaces in the collection name
-
-    
+    const deathsCollection = client.db("COVID-New").collection("RiskLevels"); 
+   
     const { date } = req.query;
+
+   
     const query = date
-      ? { date: { $regex: new RegExp(date, "i") } } // Use case-insensitive regex for string matching
+      ? { date: { $regex: new RegExp(date, "i") } } 
       : {};
 
    
     const deathsData = await deathsCollection.find(query).toArray();
 
-    
+  
     res.status(200).json(deathsData);
   } catch (error) {
     console.error("Error fetching COVID-19 risk scores:", error.message);
