a8ff842a650bce32d494d53f4e24dbf4d4cc03a5
aliyaanahmed1 <aliyaanahmed@gozzby.co.uk>
2024-12-11 14:55:52 +0000

    Update covid-riskScore.js

--- api/covid-riskScore.js
+++ api/covid-riskScore.js
@@ -16,24 +16,22 @@ module.exports = async (req, res) => {
     await client.connect();
     const deathsCollection = client.db("COVID-New").collection("Risk Scores"); // Ensure no spaces in the collection name
 
-    // Extract query parameters
+    
     const { date } = req.query;
-
-    // Build query based on the presence of the 'date' parameter
     const query = date
       ? { date: { $regex: new RegExp(date, "i") } } // Use case-insensitive regex for string matching
       : {};
 
-    // Fetch data from the database
+   
     const deathsData = await deathsCollection.find(query).toArray();
 
-    // Send the data as JSON response
+    
     res.status(200).json(deathsData);
   } catch (error) {
     console.error("Error fetching COVID-19 risk scores:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   } finally {
-    // Ensure the database connection is closed
+    
     await client.close();
   }
 };
