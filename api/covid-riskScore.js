817e99d33cebd751f5a17f8bf19f25f1a70e6e36
aliyaanahmed1 <aliyaanahmed@gozzby.co.uk>
2024-12-11 14:38:29 +0000

    Changes for risk score

---
+++ api/covid-riskScore.js
@@ -0,0 +1,29 @@
+const client = require('../db');
+
+module.exports = async (req, res) => {
+  // Add CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(204).end(); // Handle preflight requests
+    return;
+  }
+
+  try {
+    await client.connect();
+    const deathsCollection = client.db("COVID-New").collection("Risk Score");
+    const { date } = req.query;
+
+    const query = date
+      ? { "date": new RegExp(date, "i") }
+      : {};
+
+    const deathsData = await deathsCollection.find(query).toArray();
+    res.status(200).json(deathsData);
+  } catch (error) {
+    console.error("Error fetching COVID-19 deaths:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
